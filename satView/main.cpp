cd2b01b454ba8afd0f01987de72aa96793925605
devnir <to.devnir@gmail.com>
2014-05-16 16:50:00 +0300

    Актуализация перед разработкой

--- satView/main.cpp
+++ satView/main.cpp
@@ -1,7 +1,6 @@
 #include "main.h"
-#include "ui_satView.h"
+#include "ui_SatView.h"
 #include "bufffunctions.h"
-#include <QImage>
 
 
 
@@ -10,16 +9,24 @@ INT32U uid = 0;
 void (*msgOut)(INT8U *buff, INT32U l) = NULL;
 INT8U outBuff[256];
 void (*signalClose)(INT32U) = NULL;
+void updateXA1(INT8U *inBuff, INT32U len);
+BINR2xA0 pA0;
+BINR2xA1 pA1;
 
-const INT8U xC0Req[6] = "\x01\xC3\x01\x00\x00";
+const INT8U xA0Req[6] = "\x01\xA0\x02\x01\x00";
+const INT8U xA1Req[6] = "\x01\xA1\x02\x01\x00";
 
 SatView::SatView(QWidget *parent) :
   QDialog(parent),
   ui(new Ui::SatView)
 {
-  ui->setupUi(this);  
-  painter = new QPainter;
-  paintBackgr();
+  ui->setupUi(this);
+  scene = new QGraphicsScene;
+  ui->graphicsView->setScene(scene);
+  scene->setBackgroundBrush(QBrush(Qt::black));
+  scene->setSceneRect(0,0,ui->graphicsView->width(), ui->graphicsView->height());
+
+  printLogo();
 }
 
 SatView::~SatView()
@@ -50,8 +57,58 @@ extern C_CPP void initLib(void (*outFunc)(INT8U *buff,INT32U l),
   return;
 }
 
-void SatView::dataUpdate(INT8U *inBuff, INT32U l)
+void SatView::dataUpdate(INT8U *inBuff, INT32U len)
 {
+  int used = 0;
+  if(inBuff[0] == 0xA0)
+  {
+    INT32U i=0;
+    scene->clear();
+    printLogo();
+
+    for(int j = 0; j < MAX_XA0_SAT; j++)
+    {
+      pA0.sat[i].sys = 0;
+    }
+
+    pA0.satN = (len - 1)/9;
+    pA0.id = load8lu(inBuff + 0);
+    for(i =0; i< pA0.satN; i++)
+    {
+      used = 0;
+      pA0.sat[i].sys = load8lu(inBuff + 1 + (i * 9));
+      pA0.sat[i].PRN = load8lu(inBuff + 2 + (i * 9));
+      pA0.sat[i].fLiter = load8ls(inBuff + 3 + (i * 9));
+      pA0.sat[i].elev = load16lu(inBuff + 4 + (i * 9))/10;
+      pA0.sat[i].az = load16lu(inBuff + 6 + (i * 9))/10;
+      pA0.sat[i].SN = load8lu(inBuff + 8 + (i * 9))/10;
+      switch (pA0.sat[i].sys & 0x07)
+      {
+        case 1:
+          if(pA0.sat[i].PRN <33)
+          {
+            if(pA1.uGln && (0x01 << (pA0.sat[i].PRN - 1)))
+              used = 1;
+          }
+        break;
+        case 2:
+          if(pA0.sat[i].PRN <33)
+          {
+            if(pA1.uGln && (0x01 << (pA0.sat[i].PRN - 1)))
+              used = 1;
+          }
+          break;
+      }
+      printSat(pA0.sat[i].elev, pA0.sat[i].az, pA0.sat[i].sys & 0x07,
+               pA0.sat[i].PRN, pA0.sat[i].SN, used);
+      qDebug("Elev: %d", pA0.sat[i].elev);
+      qDebug("Az:   %d", pA0.sat[i].az);
+    }
+  }
+  if(inBuff[0] == 0xA1)
+  {
+    updateXA1(inBuff, len);
+  }
 }
 
 void SatView::closeEvent(QCloseEvent *)
@@ -73,9 +130,9 @@ extern C_CPP void getVersion(TPluginVersion *pl)
   pl->usedPkg = _PL_PKG_;
 }
 
-extern C_CPP void dataUpdate(INT8U *buff, INT32U l)
+extern C_CPP void dataUpdate(INT8U *buff, INT32U len)
 {
-
+  pl->dataUpdate(buff, len);
 }
 
 extern C_CPP void show()
@@ -95,77 +152,196 @@ extern C_CPP void changeStyle(QString style)
 
 void SatView::showEvent(QShowEvent *)
 {  
-
-  memcpy(outBuff, xC0Req, 5);
+  memcpy(outBuff, xA0Req, 5);
+  msgOut(outBuff, 5);
+  memcpy(outBuff, xA1Req, 5);
   msgOut(outBuff, 5);
 }
 
-void SatView::paintBackgr()
-{  //setStyleSheet("background-color: rgb(0, 0, 0)");
+void SatView::printLogo()
+{
+  QPoint p;
+  QPen   pen;
   QBrush brush;
-  QPen penB(QColor::fromRgb(65,110,25));
-  brush.setColor(Qt::red);
-  penB.setWidth(1);
+  QPixmap pix(":/LogoNVS.png");
+  scene->addPixmap(pix);
 
-  int h = this->height();
-  int w = this->width();
-
-  QRectF target(0., 0., w, h);
-  QImage image(":/LogoNVS.png");
-  QRectF source(0.0, 0.0, image.width(), image.height());
-
-  //
-  QPixmap pix( ":/LogoNVS.png" );
-
-
-  // центр
-  QPoint p2;
-  // х
-  p2.setX(w/2.);
-  // у
-  p2.setY(h/2.);
-
-  //url(:/LogoNVS.png);
-  // крест
-  painter->setPen(penB);
-  painter->drawLine(0, h/2., w, h/2.);
-  painter->drawLine(w/2., 0, w/2., h);
+  int w = ui->graphicsView->width();
+  int h = ui->graphicsView->height();
+  p.setX(w/2.);
+  p.setY(h/2.);
   // мишень
-  painter->setFont(QFont("Curier new", 10));
-  painter->drawEllipse(p2, w/2., h/2.);
-  painter->drawEllipse(p2, w/3., h/3.);
-  painter->drawEllipse(p2, w/6., h/6.);
-  // подписи осей
-  penB.setColor(Qt::green);
-  painter->setPen(penB);
-  painter->drawText(w / 2., 10, "N");
-  painter->drawText(w / 2., h - 10, "S");
-  painter->drawText(10, h / 2, "W");
-  painter->drawText(w - 10, h / 2, "E");
-  QGraphicsScene *scene = new QGraphicsScene;
-  scene->addPixmap( pix );
-  //drawImage(target, image, source);
-  ui->graphicsView->setScene(scene);
-//  ui->graphicsView->drawBackground();
+  pen.setColor(QColor::fromRgb(65, 150, 65));
+  scene->addLine(0, h/2., w, h/2., pen);
+  scene->addLine(w/2., 0, w/2., h, pen);
+
+  scene->addEllipse(0, 0, w, h, pen);
+  scene->addEllipse(w/6., h/6, w - w/3., h - h /3., pen);
+  scene->addEllipse(w/3., h/3.,2*w/6., 2*h/6., pen);
+
+
+  // подпись осей
+  QFont font = ui->graphicsView->font();
+  pen.setColor(QColor::fromRgb(0x33, 0xFF, 0xFF));
+  QPainterPath path;
+  path.addText(w/2., 10, font,  tr("N"));
+  path.addText(w/2., h-5, font, "S");
+  path.addText(0, h / 2., font, "W");
+  path.addText(w - 10., h/2., font, "E");
+  scene->addPath(path, pen);
 
 }
 
-void SatView::paintSat()
-{
-  QPainter p;
-  int elev = 17;
-  int azim = 125;
-  int h = this->height();
-  int w = this->width();
-  QBrush brush(Qt::red);
-  float r = ((90 - elev) * w / 2) / 90.;
-  int x = r * sin((180 - azim) * M_PI / 180.) + w / 2;
-  int y = r * cos((azim + 180) * M_PI / 180.) + h / 2;
-  p.setBrush(brush);
-  p.drawEllipse(x-12, y-12, 24, 24);
-}
-
-void SatView::paintEvent(QPaintEvent *ev)
+void SatView::on_pushButton_clicked()
 {
 
 }
+
+void SatView::printSat(int elev, int azim, int sys, int num, int sn, int used)
+{
+  QBrush brush(Qt::SolidPattern);
+  QColor color;
+  QPen pen;
+  float r = ((90 - elev) * ui->graphicsView->width() / 2.) / 90.;
+  float x = r * sin((180 - azim) * M_PI / 180.) + ui->graphicsView->width() / 2.;
+  float y = r * cos((azim + 180) * M_PI / 180.) + ui->graphicsView->height() / 2.;
+
+  switch(sys)
+  {
+    case 1:
+      if(sn > 0)
+        color = QColor::fromRgb(0xFF, 0, 0x00);
+      else
+        color = QColor::fromRgb(0x7F, 0, 0x00);
+      break;
+    case 2:
+      if(sn > 0)
+        color = QColor::fromRgb(0x00, 0, 0xFF);
+      else
+        color = QColor::fromRgb(0x00, 0, 0x7F);
+      break;
+    case 3:
+      if(sn > 0)
+        color = QColor::fromRgb(0x00, 0x7F, 0x00);
+      else
+        color = QColor::fromRgb(0x00, 0x3F, 0x00);
+      break;
+  }
+  if(used)
+  {
+    pen.setColor(Qt::white);
+  }
+  else
+  {
+    pen.setColor(color);
+  }
+
+  brush.setColor(color);
+  myItem *itm = new myItem;
+  itm->setPen(pen);
+  itm->setBrush(brush);
+  itm->setRect(x-12., y-12., 24., 24.);
+
+
+
+  //QGraphicsEllipseItem * item = scene->addEllipse(x-12., y-12., 24., 24., pen, brush);
+  scene->addItem(itm);
+
+   if(sn > 1)
+   {
+     color = QColor::fromRgb(0xFF, 0xFF, 0xFF);
+   }
+   else
+   {
+     color = QColor::fromRgb(0xA0, 0xA0, 0xA0);
+   }
+   pen.setColor(color);
+   pen.setWidth(1);
+   pen.setStyle(Qt::SolidLine);
+   QPainterPath path;
+   QString  str;
+
+   str.sprintf("%d", num);
+   if(num < 10)
+     path.addText(x-3, y + 3, ui->graphicsView->font(),  str);
+   else if(num < 100)
+     path.addText(x-7, y + 3, ui->graphicsView->font(),  str);
+   else
+     path.addText(x-11, y + 3, ui->graphicsView->font(),  str);
+
+   scene->addPath(path, pen);
+}
+
+
+void updateXA1(INT8U *inBuff, INT32U len)
+{
+  INT32U offset =0;
+  if(len < 3)
+    return;
+  pA1.id = load8lu(inBuff);
+  pA1.data = load16lu(inBuff + 1);
+  offset = 3;
+  if(pA1.data & 0x0001)
+  {
+    pA1.uGln = load32lu(inBuff + offset);
+    offset += 4;
+  }
+  if(pA1.data & 0x0002)
+  {
+    pA1.uGps = load32lu(inBuff + offset);
+    offset += 4;
+  }
+  if(pA1.data & 0x0004)
+  {
+    pA1.uSbas = load32lu(inBuff + offset);
+    offset += 4;
+  }
+
+  if(pA1.data & 0x0100)
+  {
+    pA1.eGln = load32lu(inBuff + offset);
+    offset += 4;
+  }
+  if(pA1.data & 0x0200)
+  {
+    pA1.eGps = load32lu(inBuff + offset);
+    offset += 4;
+  }
+  if(pA1.data & 0x0400)
+  {
+    pA1.eSbas = load32lu(inBuff + offset);
+    offset += 4;
+  }
+  return;
+}
+
+
+
+void SatView::paintEvent(QPaintEvent *)
+{
+  scene->setSceneRect(0,0,ui->graphicsView->width(), ui->graphicsView->height());
+}
+
+void SatView::resizeEvent(QResizeEvent *ev)
+{
+  resize(this->height(), this->height());
+}
+
+
+void myItem::mouseMoveEvent(QGraphicsSceneMouseEvent *event)
+{
+  QRectF r = this->rect();
+  r.setWidth(r.width() + 10);
+  r.setHeight(r.height() + 10);
+  //this->setRect(r);
+  setVisible(false);
+  update(r);
+}
+
+
+void myItem::mousePressEvent(QGraphicsSceneMouseEvent *event)
+{
+  setVisible(false);
+  update(r);
+}
+
--- satView/main.h
+++ satView/main.h
@@ -2,18 +2,22 @@
 #define SatView_H
 
 #include <QDialog>
-#include <QtCore>
-#include <QtGui>
-#include <QPixmap>
 #include "declare_global.h"
 #include "typedef.h"
-#include <QGraphicsView>
+#include <QGraphicsScene>
+#include <QPen>
+#include <QBrush>
+#include <QGraphicsItem>
+#include <QFont>
+#include <QPainterPath>
+#include <QtCore>
 
 #define _VERS_MAJOR_ 1
 #define _VERS_MINOR_ 0
 #define _PL_NAME_ "SatView"
 #define _PL_DESC_ "Plugin Description"
 #define _PL_PKG_  "0x01" //Plugin used msg
+#define MAX_XA0_SAT 128
 
 typedef struct
 {
@@ -24,10 +28,49 @@ typedef struct
   QString usedPkg;
 }TPluginVersion;
 
+typedef struct
+{
+  INT8U  sys;
+  INT8U  PRN;
+  INT8S  fLiter;
+  INT16U elev;
+  INT16U az;
+  INT16U SN;
+}TxA0Sat;
+
+typedef struct
+{
+  INT8U id;
+  TxA0Sat sat[MAX_XA0_SAT];
+  INT32U  satN;
+}BINR2xA0;
+
+typedef struct
+{
+  INT8U  id;
+  INT16U data;
+  INT32U uGln;
+  INT32U eGln;
+  INT32U uGps;
+  INT32U eGps;
+  INT32U uSbas;
+  INT32U eSbas;
+  INT32U uGll0;
+  INT32U eGll0;
+  INT32U uGll1;
+  INT32U eGll1;
+}BINR2xA1;
+
 namespace Ui {
   class SatView;
 }
 
+class myItem : public QGraphicsEllipseItem
+{
+  protected:
+    void mouseMoveEvent(QGraphicsSceneMouseEvent *event);
+    void mousePressEvent(QGraphicsSceneMouseEvent *event);
+};
 
 class SatView : public QDialog
 {
@@ -35,20 +78,23 @@ class SatView : public QDialog
 
   public:
     explicit SatView(QWidget *parent = 0);
-    void dataUpdate(INT8U *inBuff, INT32U l);
+    void dataUpdate(INT8U *inBuff, INT32U len);
     void changeStyle(QString style);
-    void paintBackgr();
-    void paintSat();
-    QPainter *painter;
+    QGraphicsScene *scene;
+    void printLogo();
+    void printSat(int elev, int azim, int sys, int num, int sn, int used);
     ~SatView();
 
   protected:
     void changeEvent(QEvent *e);
     void closeEvent(QCloseEvent *);
     void showEvent(QShowEvent *);
-    void paintEvent(QPaintEvent *ev);
+    void paintEvent(QPaintEvent *);
+    void resizeEvent(QResizeEvent *);
 
   private slots:
+
+    void on_pushButton_clicked();
 
   private:
     Ui::SatView *ui;
